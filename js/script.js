14f06186472ee2a38ae0efe712985fb29bfeb7be
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-06-05 13:42:36 +0200

    Add printMessage js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli twój ruch to ' + playerMove + ', to wygrywasz!');
