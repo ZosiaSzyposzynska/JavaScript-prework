f3d84ea157fa957933e06b388ebc365ec1e3770c
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-06-06 13:26:39 +0200

    Add result

--- js/script.js
+++ js/script.js
@@ -25,25 +25,36 @@ function playGame(playerInput) {
   displayResult(computerMove, playerMove);
 }
 
+
+
 function displayResult(argComputerMove, argPlayerMove) {
   if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
     printMessage('TY WYGRYWASZ!');
+    printResult('0-1');
   } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
     printMessage('JA WYGRYWAM!');
+    printResult('1-0')
   } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
     printMessage('TY WYGRYWASZ!');
+    printResult('0-1');
   } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
     printMessage('JA WYGRYWAM!');
+    printResult('1-0');
   } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
     printMessage('JA WYGRYWAM!');
+    printResult('1-0');
   } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
     printMessage('TY WYGRYWASZ!');
+    printResult('0-1')
   } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
     printMessage('Remis!');
+    printResult('1-1')
   } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
     printMessage('REMIS!');
+    printResult('1-1')
   } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
     printMessage('REMIS!');
+    printResult('1-1')
   }
 }
 
@@ -58,3 +69,4 @@ document.getElementById('paper-button').addEventListener('click', function () {
 document.getElementById('scissors-button').addEventListener('click', function () {
   playGame(3);
 });
+
